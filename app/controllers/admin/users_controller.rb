625542d017aa6523f543593c1b80b88f878f5fa0
Dana Woodman <dana@danawoodman.com>
2013-10-03 13:01:18 -0700

    add rfid to user admin pages

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -50,6 +50,6 @@ class Admin::UsersController < Admin::BaseController
 private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :email, :rfid_code, :password, :password_confirmation)
   end
 end
