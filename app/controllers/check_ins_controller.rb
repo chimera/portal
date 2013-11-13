a639871c1b78637c7401e04b7239388f859d8efd
Dana Woodman <dana@danawoodman.com>
2013-11-13 15:02:08 -0800

    add unlock behavior

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,7 +1,24 @@
+require 'serialport'
+
 class CheckInUser
+  class UnauthorizedRFIDCode < StandardError; end
+
   def self.call(rfid_code)
     user = User.find_by(rfid_code: rfid_code)
-    CheckIn.new(user: user)
+    if user.present?
+      unlock
+      check_in = CheckIn.create!(user: user)
+      Rails.logger.debug "User checked in with valid RFID code: #{rfid_code}"
+      check_in
+    else
+      raise UnauthorizedRFIDCode, "Code #{rfid_code} is not authorized"
+    end
+  end
+
+  def self.unlock
+    port = SerialPort.new "/dev/tty.usbmodem621", 9600
+    port.write "1\n"
+    port.close
   end
 end
 
@@ -12,14 +29,10 @@ class CheckInsController < ApplicationController
 
   def create
     check_in = CheckInUser.(params[:rfid_code])
-
-    if check_in.save
-      logger.debug "User checked in with valid RFID code: #{params[:rfid_code]}"
-      redirect_to check_ins_path, notice: "Welcome #{check_in.user.first_name}, come right on in!"
-    else
-      logger.warn "Failed Check In with RFID code: #{params[:rfid_code]}"
-      redirect_to check_ins_path, alert: 'Hey there, you can\'t come in with that kinda attitude!'
-    end
+    redirect_to check_ins_path, notice: "Welcome #{check_in.user.first_name}, come right on in!"
+  rescue CheckInUser::UnauthorizedRFIDCode
+    logger.warn "Failed Check In with RFID code: #{params[:rfid_code]}"
+    redirect_to check_ins_path, alert: 'Hey there, you can\'t come in with that kinda attitude!'
   end
 end
 
